5230d263e42140fcd08d433a9a24a9db6db518af
ursus164 <Galawieprzowa@gmail.com>
2024-12-14 22:42:07 +0100

    Moved basics typescript folder to coding repo

--- basics/with-typescript.js
+++
@@ -1,7 +0,0 @@
-// types of that function are not announced ahead of time
-function add(a, b) {
-    return a + b;
-}
-var result = add(2, 5);
-//   const result = add('2', '5');
-console.log(result);
--- basics/with-typescript.ts
+++
@@ -1,9 +0,0 @@
-// types of that function are not announced ahead of time
-function add(a : number, b : number) : number {
-    return a + b;
-  }
-  
-  const result = add(2, 5);
-//   const result = add('2', '5');
-  
-  console.log(result);
