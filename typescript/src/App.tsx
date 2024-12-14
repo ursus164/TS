b008ee011231bd56567db798a698a93fff405e82
ursus164 <Galawieprzowa@gmail.com>
2024-12-14 20:02:45 +0100

    Added todos component with props ts interface

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
+import Todos from "./components/Todos";
 
 function App() {
-
-
   return (
     <>
-      
+      <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ typescript/src/components/Todos.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// interface representing props
+interface TodosProps {
+  children: React.ReactNode;
+  items: string[];
+}
+
+const Todos = (props: TodosProps) => {
+  return (
+    <>
+      <ul>
+        {props.items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default Todos;
