566f58f86be02b391a33df3cdd9273995169efa4
ursus164 <Galawieprzowa@gmail.com>
2024-12-14 20:08:05 +0100

    Added children prop optional

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -3,7 +3,7 @@ import Todos from "./components/Todos";
 function App() {
   return (
     <>
-      <Todos />
+      <Todos items={['Learn React','Learn TypeScript']} />
     </>
   );
 }
--- typescript/src/components/Todos.tsx
+++ typescript/src/components/Todos.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // interface representing props
 interface TodosProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   items: string[];
 }
 
